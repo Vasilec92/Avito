9cc463511164d47eb259999279531f125a874234
Anna Vasileva <vasilec1992@yandex.ru>
2020-07-09 13:35:17 +0200

    2 lesson

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 'use strict';
+const dateBase = [];
 const modalAdd = document.querySelector('.modal__add'),
     addAd = document.querySelector('.add__ad'),
     modalBtnSubmit = document.querySelector('.modal__btn-submit'),
     modalSubmit = document.querySelector('.modal__submit'),
     catalog = document.querySelector('.catalog'),
-    modalItem = document.querySelector('.modal__item');
+    modalItem = document.querySelector('.modal__item'),
+    modalBtnWarning = document.querySelector('.modal__btn-warning');
+
+const elementsModalSubmit = [...modalSubmit.elements]
+    .filter(elem => elem.tagName !== 'BUTTON');
+
+    
+//стрелочная функция
+const closeModal = (event) => {
+    const target = event.target;
+    if (target.closest('.modal__close') ||
+        target === modalAdd || 
+        target === modalItem ) {
+        modalAdd.classList.add('hide');
+        modalItem.classList.add('hide');
+        modalSubmit.reset();  
+    }
+};
 
 
 addAd.addEventListener('click', ()=>{
@@ -12,29 +30,39 @@ addAd.addEventListener('click', ()=>{
     modalBtnSubmit.disabled = true ;
 });
 
-modalAdd.addEventListener('click', (event) =>{
-    const target = event.target;
-    if (target.classList.contains('modal__close') || 
-        target === modalAdd ){
-        modalAdd.classList.add('hide');
-        modalSubmit.reset();
-    }    
-});
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);
+
 
 catalog.addEventListener('click', (event)=>{
     const target = event.target;
     console.log(target);
-    if (target.classList.contains('card__image')){
-    modalItem.classList.remove('hide');
-    }   
+    if (target.closest('.card')){
+        modalItem.classList.remove('hide');
+    };   
 });
 
-modalItem.addEventListener('click', (event) =>{
-    const target = event.target;
-    if (target === modalItem || target.closest('.modal__close')){
+/*document.addEventListener('keydown', event => {
+    console.log(event);
+    if (event.code === 'Escape'){
         modalItem.classList.add('hide');
-    }
+        modalAdd.classList.add('hide');
+    };   
+}) */
+
+modalSubmit.addEventListener('input', () =>{
+    const validForm = elementsModalSubmit.every( elem => elem.value);
+    modalBtnSubmit.disabled = !validForm;
+    modalBtnWarning.style.display = validForm ? 'none' : '' ;   
 });
 
-
-
+modalSubmit.addEventListener('submit', event =>{
+    event.preventDefault();
+    const itemObj = {};
+    for (const elem of elementsModalSubmit) {
+        itemObj[elem.name] = elem.value;       
+    }   
+    dateBase.push(itemObj);
+    modalSubmit.reset();
+    console.log(dateBase);   
+});
