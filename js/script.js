a87d7dee722c3a971e06c186f964795d51dc0dbe
Anna Vasileva <vasilec1992@yandex.ru>
2020-07-11 14:34:38 +0200

    4 lesson

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 'use strict';
+
 // получаем данные из локалстораж в массив
 //const dateBase = [];
 // из стоки в обект
 // данные из локал стораж или  если их нет то пустой массив для push
 const dateBase = JSON.parse(localStorage.getItem('awito') ) || [] ; 
 //const dateBase = localStorage.getItem('awito'); 
-
+let counter = dateBase.length; 
 const modalAdd = document.querySelector('.modal__add'),
     addAd = document.querySelector('.add__ad'),
     modalBtnSubmit = document.querySelector('.modal__btn-submit'),
@@ -13,6 +14,16 @@ const modalAdd = document.querySelector('.modal__add'),
     catalog = document.querySelector('.catalog'),
     modalItem = document.querySelector('.modal__item'),
     modalBtnWarning = document.querySelector('.modal__btn-warning');
+
+const  menuContainer = document.querySelector('.menu__container');
+    // для = document.querySelector('.menu__container') карточки в окне
+const modalImageItem = document.querySelector('.modal__image-item'),
+    modalHeaderItem = document.querySelector('.modal__header-item'),
+    modalStatusItem = document.querySelector('.modal__status-item'),
+    modalDescriptionItem = document.querySelector('.modal__description-item'),
+    modalCostItem = document.querySelector('.modal__cost-item');
+
+const searchInput = document.querySelector('.search__input');
 
 const modalFileBtn = document.querySelector('.modal__file-btn'); // кнопка спан для фото чтобы сменить надпись на имя файла
 const modalImageAdd = document.querySelector('.modal__image-add');
@@ -59,11 +70,11 @@ modalFileInput.addEventListener('change', event => {
 });
 
 // создание карточек из базы данных
- const renderCard = () => {
+ const renderCard = (DB = dateBase) => {
      catalog.textContent = '';
-     dateBase.forEach((item, i) => {
+     DB.forEach(item => {
          catalog.insertAdjacentHTML('beforeend', `
-         	<li class="card" data-id="${i}">
+         	<li class="card" data-id-item="${item.id}">
 				<img class="card__image" src="data:image/jpeg;base64, ${item.photo}" alt="test">
 					<div class="card__description">
 						<h3 class="card__header">${item.nameItem}</h3>
@@ -111,9 +122,24 @@ modalItem.addEventListener('click', closeModal);
 // когда открывается окно при клике на карточку товара
 catalog.addEventListener('click', (event)=>{
     const target = event.target;
+    const card = target.closest('.card');
+
     console.log(target);
-    if (target.closest('.card')){
-        modalItem.classList.remove('hide'); // тольпри нажатии на карочку
+    // при нажатии на карту выбыраем ее ид
+    if (card){
+        console.log(card.dataset.idItem);
+        // так как ид это индекс то по нему мы обрашаемся в массиву с тим индексом
+        const item = dateBase.find(item => item.id === +card.dataset.idItem);
+        console.log(item);
+        console.log(dateBase);
+        
+        modalImageItem.src =`data:image/jpeg;base64,${item.photo}`;
+        modalHeaderItem.textContent = item.nameItem;
+        modalStatusItem.textContent = item.status === 'new' ? 'Новый': 'б/у';
+        modalDescriptionItem.textContent = item.descriptionItem;
+        modalCostItem.textContent = item.costItem;
+        
+        modalItem.classList.remove('hide'); // при нажатии на карочку
         document.addEventListener('keydown', closeModal); // закрытие при нажатии на esc
     };   
 });
@@ -133,9 +159,13 @@ modalSubmit.addEventListener('input', modalSubmit.addEventListener('input', chec
 modalSubmit.addEventListener('submit', event =>{
     event.preventDefault();
     const itemObj = {}; // создаем пустой обект
+    
     for (const elem of elementsModalSubmit) {
-        itemObj[elem.name] = elem.value;   //перебираем все елементы модального окна берем имя и значениие     
+
+        itemObj[elem.name] = elem.value;   //перебираем все елементы модального окна берем имя и значениие  
+        
     }
+    itemObj.id = counter++;
     // добавляем фото в обект
     itemObj.photo = infoPhoto.base64;
     // добавляем обект в массив
@@ -148,6 +178,30 @@ modalSubmit.addEventListener('submit', event =>{
     
 });
 
+//обрезать пробелы трим и привести к нижнему регистру
+searchInput.addEventListener('input', () =>{
+    const valueSearch = searchInput.value.trim().toLowerCase();
+    // поиск от 2 символов
+    if (valueSearch.length > 2){
+        const result = dateBase.filter(item => item.nameItem.toLowerCase().includes(valueSearch) || 
+                                            item.descriptionItem.toLowerCase().includes(valueSearch));
+        //передаем бызу данных на вывод
+        renderCard(result);
+    }
 
+});
+
+menuContainer.addEventListener('click', event =>{
+    const target = event.target;
+
+    if(target.tagName === 'A'){
+        const result = dateBase.filter(item => item.category === target.dataset.category);
+        renderCard(result);
+
+    }
+
+})
 
 renderCard();
+
+
